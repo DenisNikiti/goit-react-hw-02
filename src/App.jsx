93c07ff5a82ef381b69f3e9ac035e67af86e5daf
DenisNikiti <den.hikitin@gmail.com>
2024-02-16 20:12:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useState } from "react";
 import Description from "./components/Description/Description";
 
 function App() {
-  // const [feedbacks, setfeedbacks] = useState({
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // });
-
   return (
     <div>
       <Description />
